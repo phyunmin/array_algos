9a2ef367c92e1365229c0d539211007014eac381
Hyunmin Park <hyunmin.park.career@gmail.com>
2020-02-19 11:42:51 -0800

    new algo

--- 4_extra_practice.js
+++ 4_extra_practice.js
@@ -4,10 +4,29 @@
 // YOUR CODE HERE
 
 
+// var arr = [8,6,7,5,3,0,9];
+// var newArr = [];
+// for(var i=0; i<arr.length; i++){
+//     newArr.push(arr[arr.length-i-1]);
+// }
+// console.log(arr);
+// console.log(newArr);
+
+
+// var arr = [8,6,7,5,3,0,9];
+// var newArr = [];
+// var copyArr = arr.slice();
+// for(var i=0; i<arr.length; i++){
+//     newArr.push(copyArr.pop());
+// }
+// console.log(arr);
+// console.log(newArr);
+
 var arr = [8,6,7,5,3,0,9];
-var newArr = [];
-for(var i=0; i<arr.length; i++){
-    newArr.push(arr[arr.length-i-1]);
+var midIndex = arr.length/2;
+for(var i=0; i<midIndex; i++){
+    var temp = arr[i];
+    arr[i] = arr[arr.length-1-i];
+    arr[arr.length-1-i] = temp;
 }
 console.log(arr);
-console.log(newArr);
